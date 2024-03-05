fc21002ee5f2993eb6bd646d79727b333f1ac705
luana-garcia <luanaqg@estudante.ufscar.br>
2024-03-05 18:26:53 -0300

    Conectando chat bot com código de nlp

--- nlp.py
+++ nlp.py
@@ -1,17 +1,15 @@
 import google.generativeai as genai
 
 # https://aistudio.google.com/app/apikey
-from credential import GOOGLE_API_KEY
+import env
 
-genai.configure(api_key=GOOGLE_API_KEY)
+def nlp_response (text):
+    genai.configure(api_key=env.GOOGLE_API_KEY)
 
-for m in genai.list_models():
-  if 'generateContent' in m.supported_generation_methods:
-    print(m.name)
+    for m in genai.list_models():
+        if 'generateContent' in m.supported_generation_methods:
+            print(m.name)
 
-model = genai.GenerativeModel('gemini-pro')
-response = model.generate_content("What is the meaning of life?")
-print(response.text)
-
-# Speech Analysis
-print(response.prompt_feedback)
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(text)
+    return response.text
